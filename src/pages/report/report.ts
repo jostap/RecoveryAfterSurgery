531c7f595becd0006f9c244a0e568fef9587b459
Jose Tapia <jose.tapia@ki.se>
2018-10-05 17:00:04 +0200

    Add button Send Report to Page report

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ReportPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-report',
@@ -20,6 +13,10 @@ export class ReportPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReportPage');
+  }
+
+  sendReport(){
+    console.log("Send Report");
   }
 
 }
