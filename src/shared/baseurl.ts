d132481d2058cdf69aa428e381f2169ecc5533f8
cujoproject <kemisten@gmail.com>
2018-12-18 12:33:49 +0100

    Reinstall RaS

--- src/shared/baseurl.ts
+++ src/shared/baseurl.ts
@@ -1,3 +1,3 @@
-//export const baseURL = 'http://127.0.0.1:10010/';
+export const baseURL = 'http://127.0.0.1:10010/';
 
-export const baseURL = 'https://medredserver.herokuapp.com/';
+//export const baseURL = 'https://medredserver.herokuapp.com/';
